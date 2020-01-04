156e6d008e0620ea19b0961fa69c28dc484a7d49
aquila <t1@constant-progress.de>
2020-01-04 17:22:27 +0100

    Create getTacoSub.sh

---
+++ data/getTacoSub.sh
@@ -0,0 +1,10 @@
+filename="coco_do.zip"
+fileid="1fBtBkqIF0sNzyQjRenfIiugZVQPhJok4"  # coco.zip
+
+# Download from Google Drive, accepting presented query
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+rm ./cookie
+
+# Unzip
+unzip -q ${filename}  # for coco_do.zip
